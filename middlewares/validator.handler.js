3ea22961321abf5490e6edfbae14098e77539aca
Guillo <guillermoivansalgadosandoval@gmail.com>
2024-07-16 03:22:16 -0500

    Alerts added

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -3,7 +3,8 @@ import boom from "@hapi/boom";
 export function validatorHandler (schema, property) {
     return (req, res, next) => {
         const data = req[property]
-        const { error } = schema.validate(data, { abortEarly: false })
+        const { error } = schema.validate(data)
+        //const { error } = schema.validate(data, { abortEarly: false })
         if (error) {
             next(boom.badRequest(error))
         }
--- views/src/js/sendData.js
+++ views/src/js/sendData.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const registerForm = document.getElementById('registerForm');
+    const alertContainer = document.getElementById('alert-container');
 
     registerForm.addEventListener('submit', async (event) => {
         event.preventDefault(); // Evitar el envío del formulario por defecto
@@ -25,15 +26,29 @@ document.addEventListener('DOMContentLoaded', () => {
             const responseData = await response.json();
 
             if (!response.ok) {
-                throw new Error(responseData.message || 'Unknown error occurred');
+                //throw new Error(responseData.message || 'Unknown error occurred');
+                alertContainer.innerHTML = `
+                <div class="alert alert-warning d-flex align-items-center" role="alert">
+                    <div>
+                        ${responseData.message}
+                    </div>
+                    <button type="button" class="btn-close" aria-label="Close"></button>
+                </div>
+            `;
+            const closeButton = alertContainer.querySelector('.btn-close');
+    if (closeButton) {
+        closeButton.addEventListener('click', () => {
+            // Eliminar la alerta del DOM
+            alertContainer.innerHTML = '';
+        });
+    }
+            }else {
+                console.log(responseData);
+                window.location.href = "https://register-q25e.onrender.com/home";
             }
-
-            console.log(responseData);
-            window.location.href = "https://register-q25e.onrender.com/home";
             // Aquí puedes manejar la respuesta del servidor como sea necesario
         } catch (error) {
             console.error('Error:', error.message || error);
-            // Manejar el error aquí
         }
     });
 });
